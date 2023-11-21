15a6ec5a84cc44411ff3de54f1477dc9b555bede
Austin Labador <austinlabador@gmail.com>
2023-11-21 02:20:50 -0800

    Setup firebase config

---
+++ backend/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import {initializeApp} from "firebase/auth"
+import 'dotenv/config'
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = app.auth()
+export default app 
