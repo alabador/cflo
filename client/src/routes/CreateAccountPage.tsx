d8fa0447f544c16bbb85e2ca9067dc55f0551ba6
Austin Labador <austinlabador@gmail.com>
2023-12-14 02:01:57 -0800

    Change header style for account sign up page

--- client/src/routes/CreateAccountPage.tsx
+++ client/src/routes/CreateAccountPage.tsx
@@ -6,11 +6,11 @@ const CreateAccountPage = ({tokenValue, authStatus, isAuthenticated}) => {
   return (
     <main className='w-full min-h-screen flex flex-col justify-center items-center'>
         <ThemeController />
-        <h1>Create an Account</h1>
+        <h1 className='mb-8 mt-2 text-2xl font-bold'>Create an Account</h1>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <SignUp tokenValue={tokenValue} authStatus={authStatus} isAuthenticated={isAuthenticated}/>
-            <div className="divider my-0"></div> 
-            <SignInCta />
+          <SignUp tokenValue={tokenValue} authStatus={authStatus} isAuthenticated={isAuthenticated}/>
+          <div className="divider my-0"></div> 
+          <SignInCta />
         </div>
     </main>
   )
