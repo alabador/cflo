3104c77e694e07c1e3c89e59810f9a7630da004a
Austin Labador <austinlabador@gmail.com>
2023-12-16 03:24:45 -0800

    Add expense schema

---
+++ backend/models/expense.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const expenseSchema = new Schema({
+    expense_name : {
+        type: String,
+        required: true
+    },
+    expense_price : {
+        type: Number,
+        required: true
+    },
+    expense_category : {
+        type: String,
+        required: false
+    },
+    is_expense: {
+        type: Boolean,
+        required: true
+    }
+}, {timestamps : true})
+
+const Expense = mongoose.model('expense', expenseSchema)
+
+export default Expense
